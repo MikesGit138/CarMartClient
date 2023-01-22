a5d874b6df2b0ba731b0adc4fba82cbbe63b7acd
bombies <xjamesdzn@gmail.com>
2023-01-22 12:08:43 -0500

    Add login button to login route. Changed sidebar icon for welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/components/*.{html,ts}",
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/components/*.{html,ts}", "./src/**/*.{html,ts}"],
   darkMode: true,
   theme: {
+    screens: {
+      laptop: { max: "1280px" },
+      tablet: { max: "1025px" },
+      phone: { max: "615px" },
+      "laptop-min": { min: "1280px" },
+      "tablet-min": { min: "1025px" },
+      "phone-min": { min: "615px" },
+    },
     extend: {
       colors: {
-        'secondary' : '#1E1E1E',
-        'primary' : '#3b82f6',
-        'secondary-text' : 'rgb(115,115,115)'
+        secondary: "#1E1E1E",
+        primary: "#3b82f6",
+        "secondary-text": "rgb(115,115,115)",
       },
     },
   },
   plugins: [],
-}
+};
