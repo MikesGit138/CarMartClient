5ceb3662915ea182070ad3b35552c850e8b31f4c
Michael Leighton <michael138leighton@gmail.com>
2023-01-23 17:42:50 -0500

    sidebar moved to it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     WelcomeComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
